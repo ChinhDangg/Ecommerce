16e4839e4b117be640fd2c47c32fde8105a33081
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-03-20 23:15:52 -0400

    add product category dto

--- src/main/java/dev/ecommerce/product/DTO/ProductCategoryDTO.java
+++ src/main/java/dev/ecommerce/product/DTO/ProductCategoryDTO.java
@@ -1,13 +1,20 @@
 package dev.ecommerce.product.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class ProductCategoryDTO {
+    @Min(value = 1, message = "Category value must be positive")
     private Integer id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 2 and 30 characters")
     private String name;
 }
