461ba50bc7914b5527acb3ab1bc0bad96c3d5117
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-03-21 22:41:58 -0400

    add global exception handler

---
+++ src/main/java/dev/ecommerce/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package dev.ecommerce.exceptionHandler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
