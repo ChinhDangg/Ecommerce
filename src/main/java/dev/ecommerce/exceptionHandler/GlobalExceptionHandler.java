f2c263762d4888daa3d18ba9bf529f82acc5238a
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-05-11 17:00:27 -0400

    add more exceptions

--- src/main/java/dev/ecommerce/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/dev/ecommerce/exceptionHandler/GlobalExceptionHandler.java
@@ -2,20 +2,32 @@ package dev.ecommerce.exceptionHandler;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body("Fail validation for fields");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body("Required request body is missing or mismatched type");
+    }
+
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.badRequest().body("Error Data Integrity: " + e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.badRequest().body("Error Runtime: " + e.getMessage());
     }
 
 }
