4ee09136136f83afe909992a44bd6fa12a5d5f82
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-05-16 14:52:06 -0400

    add some testing

--- src/main/java/dev/ecommerce/EcommerceApplication.java
+++ src/main/java/dev/ecommerce/EcommerceApplication.java
@@ -1,15 +1,19 @@
 package dev.ecommerce;
 
-import dev.ecommerce.product.DTO.ProductCategoryDTO;
-import dev.ecommerce.product.entity.ProductCategory;
+import dev.ecommerce.product.DTO.ProductLineDTO;
+import dev.ecommerce.product.entity.ContentType;
+import dev.ecommerce.product.entity.ProductLine;
+import dev.ecommerce.product.entity.ProductLineDescription;
+import dev.ecommerce.product.entity.ProductLineMedia;
 import dev.ecommerce.product.repository.ProductCategoryRepository;
+import dev.ecommerce.product.repository.ProductLineDescriptionRepository;
+import dev.ecommerce.product.repository.ProductLineMediaRepository;
+import dev.ecommerce.product.repository.ProductLineRepository;
+import dev.ecommerce.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -18,36 +22,41 @@ public class EcommerceApplication {
         SpringApplication.run(EcommerceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(ProductCategoryRepository productCategoryRepository) {
-        return _ -> {
-            ProductCategory electronics = new ProductCategory("Electronics", null);
-
-            ProductCategory computers = new ProductCategory("Computers", electronics);
-
-            ProductCategory laptops = new ProductCategory("Laptop", computers);
-
-            ProductCategory gamingLaptops = new ProductCategory("GamingLaptops", laptops);
-
-            ProductCategory macs = new ProductCategory("Mac", laptops);
-
-            ProductCategory computerComponent= new ProductCategory("Computer Component", electronics);
-
-            ProductCategory graphicsCard = new ProductCategory("Graphics Card", computerComponent);
-
-            productCategoryRepository.save(electronics);
-            productCategoryRepository.save(computers);
-            productCategoryRepository.save(laptops);
-            productCategoryRepository.save(gamingLaptops);
-            productCategoryRepository.save(macs);
-            productCategoryRepository.save(computerComponent);
-            productCategoryRepository.save(graphicsCard);
-
-            List<ProductCategoryDTO> parentProductCategories = productCategoryRepository.findAllTopParentCategory();
-            System.out.println(parentProductCategories.getFirst().getId());
-            System.out.println(parentProductCategories.getFirst().getName());
-
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            ProductCategoryRepository productCategoryRepository,
+//            ProductLineRepository productLineRepository,
+//            ProductLineDescriptionRepository productLineDescriptionRepository,
+//            ProductLineMediaRepository productLineMediaRepository,
+//            ProductService productService
+//            ) {
+//        return _ -> {
+//            ProductCategory electronics = new ProductCategory("Electronics", null);
+//
+//            ProductCategory computers = new ProductCategory("Computers", electronics);
+//
+//            ProductCategory laptops = new ProductCategory("Laptop", computers);
+//
+//            ProductCategory gamingLaptops = new ProductCategory("GamingLaptops", laptops);
+//
+//            ProductCategory macs = new ProductCategory("Mac", laptops);
+//
+//            ProductCategory computerComponent= new ProductCategory("Computer Component", electronics);
+//
+//            ProductCategory graphicsCard = new ProductCategory("Graphics Card", computerComponent);
+//
+//            productCategoryRepository.save(electronics);
+//            productCategoryRepository.save(computers);
+//            productCategoryRepository.save(laptops);
+//            productCategoryRepository.save(gamingLaptops);
+//            productCategoryRepository.save(macs);
+//            productCategoryRepository.save(computerComponent);
+//            productCategoryRepository.save(graphicsCard);
+//
+//            List<ProductCategoryDTO> parentProductCategories = productCategoryRepository.findAllTopParentCategory();
+//            System.out.println(parentProductCategories.getFirst().getId());
+//            System.out.println(parentProductCategories.getFirst().getName());
+//        };
+//    }
 
 }
--- src/test/java/dev/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/dev/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,85 @@
 package dev.ecommerce;
 
+import dev.ecommerce.product.DTO.ProductLineDTO;
+import dev.ecommerce.product.DTO.ProductMapper;
+import dev.ecommerce.product.entity.ContentType;
+import dev.ecommerce.product.entity.ProductLine;
+import dev.ecommerce.product.entity.ProductLineDescription;
+import dev.ecommerce.product.entity.ProductLineMedia;
+import dev.ecommerce.product.repository.ProductLineRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.Optional;
+
+@DataJpaTest
+@ActiveProfiles("test") // use H2 config here
 class EcommerceApplicationTests {
 
+    @Autowired
+    private ProductLineRepository productLineRepository;
+
+    private final ProductMapper productMapper = Mappers.getMapper(ProductMapper.class);
+
     @Test
-    void contextLoads() {
+    void testFindProductLineWithDescriptionsAndMedia() {
+        ProductLine productLine = new ProductLine("Product line name");
+
+        ProductLineDescription description = new ProductLineDescription(productLine, ContentType.IMAGE, "desc image", 0);
+        ProductLineMedia media = new ProductLineMedia(productLine, ContentType.IMAGE, "media image", 0);
+
+        productLine.getDescriptions().add(description);
+        productLine.getMedia().add(media);
+
+        productLineRepository.save(productLine);
+
+        Optional<ProductLine> resultOpt = productLineRepository.findById(productLine.getId());
+        Assertions.assertTrue(resultOpt.isPresent());
+
+        ProductLine result = resultOpt.get();
+
+        Assertions.assertEquals(1, result.getDescriptions().size());
+        Assertions.assertEquals("desc image", result.getDescriptions().getFirst().getContent());
+
+        Assertions.assertEquals(1, result.getMedia().size());
+        Assertions.assertEquals("media image", result.getMedia().getFirst().getContent());
     }
+
+    @Test
+    void testProductLineMapper() {
+        ProductLine productLine = new ProductLine("Product line name");
+        ProductLineDescription description = new ProductLineDescription(
+                productLine,
+                ContentType.IMAGE,
+                "some image name",
+                0
+        );
+
+        ProductLineMedia media = new ProductLineMedia(
+                productLine,
+                ContentType.IMAGE,
+                "some image name",
+                0
+        );
+
+        productLine.getDescriptions().add(description);
+        productLine.getMedia().add(media);
+
+        ProductLineDTO productLineDTO = productMapper.toDTO(productLine);
+        Assertions.assertEquals(productLine.getName(), productLineDTO.getName());
+        Assertions.assertEquals(productLine.getDescriptions().size(), productLineDTO.getDescriptions().size());
+        Assertions.assertEquals(productLine.getMedia().size(), productLineDTO.getMedia().size());
+        Assertions.assertEquals(
+                productLine.getDescriptions().getFirst().getContent(),
+                productLineDTO.getDescriptions().getFirst().content()
+        );
+
+    }
+
+
 
 }
