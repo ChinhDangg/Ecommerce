d5f0b1b6aec9222c434cd2e2ccdcfc9136bb1e9a
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-05-16 14:55:16 -0400

    add product jpa specification for complex query

---
+++ src/main/java/dev/ecommerce/product/repository/ProductSpecifications.java
@@ -0,0 +1,26 @@
+package dev.ecommerce.product.repository;
+
+import dev.ecommerce.product.entity.Product;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecifications {
+
+    public static Specification<Product> nameContainsWords(String search) {
+        return (root, query, cb) -> {
+            if (search == null) {
+                return cb.conjunction();
+            }
+            String refined = search.replaceAll("[^a-zA-Z0-9 ]", "");
+            if (refined.isEmpty()) {
+                return cb.conjunction();
+            }
+            String[] words = refined.toLowerCase().split("\\s+");
+            Predicate[] predicates = new Predicate[words.length];
+            for (int i = 0; i < words.length; i++) {
+                predicates[i] = cb.like(cb.lower(root.get("name")), "%" + words[i] + "%");
+            }
+            return cb.and(predicates);
+        };
+    }
+}
