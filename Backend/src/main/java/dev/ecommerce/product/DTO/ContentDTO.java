7656938728a004e0e34cefe3c4f6987932f7e018
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-05-29 16:05:52 -0400

    add id to contentDTO to handle update

--- Backend/src/main/java/dev/ecommerce/product/DTO/ContentDTO.java
+++ Backend/src/main/java/dev/ecommerce/product/DTO/ContentDTO.java
@@ -2,4 +2,7 @@ package dev.ecommerce.product.DTO;
 
 import dev.ecommerce.product.entity.ContentType;
 
-public record ContentDTO(ContentType contentType, String content) {}
+public record ContentDTO(
+        Long id,
+        ContentType contentType,
+        String content) {}
