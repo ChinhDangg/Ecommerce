457be51aca2fbaa9eaf0645149961ba84339381a
Chinh Dang <dangchinh645@gmail.com>
2025-06-28 18:52:10 -0400

    add search by name and by category

--- Backend/src/main/java/dev/ecommerce/product/repository/ProductRepository.java
+++ Backend/src/main/java/dev/ecommerce/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package dev.ecommerce.product.repository;
 
 import dev.ecommerce.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(value = "select id from products where product_line_id = :id", nativeQuery = true)
     Optional<Long[]> findAllIdByProductLineId(Integer id);
+
+    Page<Product> findByCategoryId(Integer id, Pageable pageable);
 }
