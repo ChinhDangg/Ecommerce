81aeaaa08ee220ca8bc210c8e4b3feddf9dc35ed
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-08-08 22:31:34 -0400

    refactor image controller to use image service

--- Backend/src/main/java/dev/ecommerce/product/controller/ImageController.java
+++ Backend/src/main/java/dev/ecommerce/product/controller/ImageController.java
@@ -1,8 +1,8 @@
 package dev.ecommerce.product.controller;
 
+import dev.ecommerce.product.service.MediaService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @RestController
 @RequestMapping("/images")
 public class ImageController {
 
+    private final MediaService mediaService;
+
+    public ImageController(MediaService mediaService) {
+        this.mediaService = mediaService;
+    }
+
     @GetMapping("/{filename}")
     public ResponseEntity<Resource> getImage(@PathVariable String filename, HttpServletRequest request) {
         try {
-            String IMAGE_DIR = "C:\\Users\\admin\\IdeaProjects\\Ecommerce\\Backend\\uploads\\images\\";
-
-            Path filePath = Paths.get(IMAGE_DIR).resolve(filename).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
+            Resource resource = mediaService.getImageResource(filename);
 
             if (!resource.exists()) {
                 System.out.println("File not found: " + filename);
