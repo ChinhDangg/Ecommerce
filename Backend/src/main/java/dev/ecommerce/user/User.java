c1c03762cfee966e237ff676d9c711c3cd876c8c
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-08-13 22:34:56 -0400

    add user entity

---
+++ Backend/src/main/java/dev/ecommerce/user/User.java
@@ -0,0 +1,30 @@
+package dev.ecommerce.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+}
