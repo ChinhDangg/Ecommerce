ad639cbb8ea68f6a87cfaeaec63775d7949bcbff
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2025-08-20 22:23:37 -0400

    add product cart dto

---
+++ Backend/src/main/java/dev/ecommerce/product/DTO/ProductCartDTO.java
@@ -0,0 +1,19 @@
+package dev.ecommerce.product.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCartDTO {
+        private List<ShortProductDTO> productList;
+        private int totalQuantity;
+        private BigDecimal taxAmount;
+        private BigDecimal totalPrice;
+        private BigDecimal priceAfterTax;
+}
