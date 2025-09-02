13b8095058158a43acc14a4bde070f548ad90927
chinhdang <dangchinh645@gmail.com>
2025-09-01 23:03:03 -0400

    add checkout service

---
+++ Backend/src/main/java/dev/ecommerce/checkout/CheckoutService.java
@@ -0,0 +1,81 @@
+package dev.ecommerce.checkout;
+
+import dev.ecommerce.product.entity.Product;
+import dev.ecommerce.user.constant.UserItemType;
+import dev.ecommerce.user.entity.UserItem;
+import dev.ecommerce.user.repository.UserItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CheckoutService {
+
+    private final UserItemRepository userItemRepository;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final RedisScript<List> reserveScript;
+
+    private static String stockKey(long pid) { return "stock:{" + pid + "}"; }
+    private static String holdsKey(long pid) { return "holds:{" + pid + "}"; }
+    private static String expKey(long pid)   { return "exp:{" + pid + "}"; }
+
+    @Transactional(readOnly = true)
+    public ReserveStatus reserve(Long userId) {
+        List<UserItem> carts = userItemRepository.getUserItemsByUserId(userId);
+
+        for (UserItem cart : carts) {
+            if (cart.getType() == UserItemType.SAVED)
+                continue;
+            ReserveResult result = reserveWithCache(cart.getProduct().getId(), cart.getId(), cart.getQuantity());
+            if (result.status() == ReserveStatus.NO_CACHE) {
+                Product product = cart.getProduct();
+                addProductQuantityCache(product.getId(), product.getQuantity());
+                ReserveResult retry =  reserveWithCache(product.getId(), cart.getId(), cart.getQuantity());
+                return retry.status();
+            }
+            return result.status();
+        }
+
+        return ReserveStatus.EMPTY;
+    }
+
+    private void addProductQuantityCache(long productId, int quantity) {
+        stringRedisTemplate.opsForValue().set(
+                stockKey(productId),
+                String.valueOf(quantity),
+                Duration.ofHours(1)
+        );
+    }
+
+    private ReserveResult reserveWithCache(Long productId, long cartId, int requestQuantity) {
+        long exp = System.currentTimeMillis() + Duration.ofMinutes(30).toMillis();
+
+        List<String> keys = List.of(stockKey(productId), holdsKey(productId), expKey(productId));
+        List<String> args = List.of(String.valueOf(cartId), String.valueOf(requestQuantity), String.valueOf(exp));
+
+        @SuppressWarnings("unchecked")
+        List<Object> result = (List<Object>) stringRedisTemplate.execute(reserveScript, keys, args.toArray());
+
+        if (result.size() != 2)
+            return new ReserveResult(ReserveStatus.BAD_REQUEST, -3);
+        long status = ((Number) result.getFirst()).longValue();
+        long value =  ((Number) result.get(1)).longValue();
+
+        if ((int) status == 1) {
+            return new ReserveResult(ReserveStatus.OK, value);
+        } else {
+            return switch ((int) value) {
+                case -1 -> new ReserveResult(ReserveStatus.NOT_ENOUGH, -1);
+                case -2 -> new ReserveResult(ReserveStatus.NO_CACHE, -2);
+                default -> new ReserveResult(ReserveStatus.BAD_REQUEST, value);
+            };
+        }
+    }
+
+}
