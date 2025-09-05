67d1d06111d2ce4710d67150033167443fdc2459
chinhdang <dangchinh645@gmail.com>
2025-09-04 23:16:28 -0400

    refactor code and add place order endpoint

--- Backend/src/main/java/dev/ecommerce/order/controller/OrderController.java
+++ Backend/src/main/java/dev/ecommerce/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package dev.ecommerce.order.controller;
 
 import dev.ecommerce.order.service.CheckoutService;
 import dev.ecommerce.order.constant.ReserveStatus;
+import dev.ecommerce.order.service.OrderService;
 import dev.ecommerce.user.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/check-out")
+@RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
 
     private final CheckoutService checkoutService;
 
+    private Long getUserId(Authentication authentication) {
+        if (authentication == null)
+            return null;
+        return ((SecurityUser) authentication.getPrincipal()).user().getId();
+    }
+
     @GetMapping("/reserve")
     public ResponseEntity<String> reserve(Authentication authentication) {
-        Long userId = ((SecurityUser) authentication.getPrincipal()).user().getId();
+        Long userId = getUserId(authentication);
         ReserveStatus status = checkoutService.reserve(userId);
         if (status == ReserveStatus.OK) {
             return ResponseEntity.ok("OK");
         } else {
             return ResponseEntity.badRequest().body(status.name());
         }
+    }
+
+    @GetMapping("/place")
+    public ResponseEntity<Long> placeOrder(Authentication authentication) {
+        Long userId = getUserId(authentication);
+        return ResponseEntity.ok().body(checkoutService.placeOrder(userId));
     }
 }
