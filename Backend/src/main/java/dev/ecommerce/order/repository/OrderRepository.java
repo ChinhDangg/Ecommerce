c0dfe1bcd96a6ce399d79d022fa9f9af7ae7f908
chinhdang <dangchinh645@gmail.com>
2025-09-04 23:15:35 -0400

    update getting user order info to have time filter option

--- Backend/src/main/java/dev/ecommerce/order/repository/OrderRepository.java
+++ Backend/src/main/java/dev/ecommerce/order/repository/OrderRepository.java
@@ -1,17 +1,22 @@
 package dev.ecommerce.order.repository;
 
 import dev.ecommerce.order.entity.Order;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findAllByUserIdOrderByPlacedAtDesc(Long id, Instant cutOff, Pageable pageable);
+    Page<Order> findByUserIdAndPlacedAtGreaterThanEqualAndPlacedAtLessThanOrderByPlacedAtDesc(Long id, Instant start, Instant end,  Pageable pageable);
 
-    long countAllByUserIdOrderByPlacedAtDesc(Long id, Instant cutOff);
+    long countAllByUserIdAndPlacedAtGreaterThanEqualAndPlacedAtLessThanOrderByPlacedAtDesc(Long id, Instant start, Instant end);
+
+    @Query("SELECT MIN(o.placedAt) FROM Order o WHERE o.user.id = :userId")
+    Instant findOldestPlacedAtByUserId(@Param("userId") Long userId);
 }
