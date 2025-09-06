53b0f4fdf0f8f15140a5511b63d12f1e648aebb9
chinhdang <dangchinh645@gmail.com>
2025-09-06 08:18:32 -0400

    add get user history order endpoint

--- Backend/src/main/java/dev/ecommerce/userInfo/controller/UserController.java
+++ Backend/src/main/java/dev/ecommerce/userInfo/controller/UserController.java
@@ -3,7 +3,9 @@ package dev.ecommerce.userInfo.controller;
 import dev.ecommerce.product.DTO.ProductCartDTO;
 import dev.ecommerce.user.SecurityUser;
 import dev.ecommerce.userInfo.DTO.UserCartDTO;
+import dev.ecommerce.userInfo.DTO.UserOrderHistory;
 import dev.ecommerce.userInfo.service.UserItemService;
+import dev.ecommerce.userInfo.service.UserOrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,12 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserItemService userItemService;
+    private final UserOrderService userOrderService;
 
     private Long getUserId(Authentication authentication) {
         if (authentication == null)
@@ -64,5 +69,13 @@ public class UserController {
     public ResponseEntity<?> savedToCart(@RequestParam Long productId, Authentication authentication) {
         userItemService.moveProductFromSavedToCart(getUserId(authentication), productId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/order/history")
+    public ResponseEntity<UserOrderHistory> getUserOrderHistory(@RequestParam Instant start,
+                                                                @RequestParam Instant end,
+                                                                @RequestParam int page, Authentication authentication) {
+        Long userId = getUserId(authentication);
+        return ResponseEntity.ok().body(userOrderService.getUserOrderHistory(userId, start, end, page, 10));
     }
 }
