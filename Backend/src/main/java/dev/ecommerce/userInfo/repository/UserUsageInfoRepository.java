98928fd2e8edf41e9decfee5d287349a6374f87d
chinhdang <dangchinh645@gmail.com>
2025-09-05 22:13:57 -0400

    add find by user id

--- Backend/src/main/java/dev/ecommerce/userInfo/repository/UserUsageInfoRepository.java
+++ Backend/src/main/java/dev/ecommerce/userInfo/repository/UserUsageInfoRepository.java
@@ -3,6 +3,9 @@ package dev.ecommerce.userInfo.repository;
 import dev.ecommerce.userInfo.entity.UserUsageInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserUsageInfoRepository extends JpaRepository<UserUsageInfo, Long> {
 
+    Optional<UserUsageInfo> findByUserId(Long userId);
 }
