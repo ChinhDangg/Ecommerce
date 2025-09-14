a8760ba8efc37d5bac9274b16c8deb1a68e7ecd5
chinhdang <dangchinh645@gmail.com>
2025-09-14 19:29:22 -0400

    add checkout dto

---
+++ Backend/src/main/java/dev/ecommerce/orderProcess/model/CheckoutDTO.java
@@ -0,0 +1,16 @@
+package dev.ecommerce.orderProcess.model;
+
+import dev.ecommerce.product.DTO.ProductCartDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class CheckoutDTO {
+    private String username;
+    private String address;
+    private ProductCartDTO productInfo;
+    private Map<Long, Map<String, Long>> reserveInfo;
+}
