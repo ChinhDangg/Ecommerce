f3743174fee26834a9ecbcfeb5c438236c629aa0
chinhdang <dangchinh645@gmail.com>
2025-09-22 19:19:12 -0400

    add edit address service logic

--- Backend/src/main/java/dev/ecommerce/userInfo/service/UserInfoService.java
+++ Backend/src/main/java/dev/ecommerce/userInfo/service/UserInfoService.java
@@ -5,7 +5,10 @@ import dev.ecommerce.exceptionHandler.ResourceNotFoundException;
 import dev.ecommerce.user.SecurityUser;
 import dev.ecommerce.user.entity.User;
 import dev.ecommerce.user.repository.UserRepository;
+import dev.ecommerce.userInfo.DTO.UserAddress;
 import dev.ecommerce.userInfo.DTO.UserBasicInfo;
+import dev.ecommerce.userInfo.entity.UserUsageInfo;
+import dev.ecommerce.userInfo.repository.UserUsageInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
 import org.springframework.http.ResponseCookie;
@@ -19,8 +22,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserInfoService {
 
     private final UserRepository userRepository;
+    private final UserItemService userItemService;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final UserUsageInfoRepository userUsageInfoRepository;
 
     public User findUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(
@@ -81,6 +86,23 @@ public class UserInfoService {
 
         user.setPassword(passwordEncoder.encode(userBasicInfo.getNewPass()));
         return userRepository.save(user).getFirstname();
+    }
+
+    public UserAddress getAddress(Long userId) {
+        return userItemService.findUserInfoByUserId(userId).getUserAddressDetails();
+    }
+
+    @Transactional
+    public UserAddress updateAddress(Long userId, UserAddress userAddress) {
+        UserUsageInfo userInfo = userItemService.findUserInfoByUserId(userId);
+        userInfo.setAddress(
+                userAddress.street(),
+                userAddress.city(),
+                userAddress.state(),
+                userAddress.zipcode(),
+                userAddress.country()
+        );
+        return userUsageInfoRepository.save(userInfo).getUserAddressDetails();
     }
 
 }
