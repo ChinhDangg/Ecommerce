b64ae52db8c26cd97babd4e745379a8013976761
chinhdang <dangchinh645@gmail.com>
2025-09-21 23:43:05 -0400

    fix relationship

--- Backend/src/main/java/dev/ecommerce/orderProcess/entity/OrderItem.java
+++ Backend/src/main/java/dev/ecommerce/orderProcess/entity/OrderItem.java
@@ -24,7 +24,7 @@ public class OrderItem {
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
