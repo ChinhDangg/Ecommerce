1dc05e5b71decd25e8383bdd0258607b0236a58c
chinhdang <dangchinh645@gmail.com>
2025-09-24 11:44:37 -0400

    change record to class

--- Backend/src/main/java/dev/ecommerce/userInfo/DTO/UserProductReviewInfo.java
+++ Backend/src/main/java/dev/ecommerce/userInfo/DTO/UserProductReviewInfo.java
@@ -3,16 +3,28 @@ package dev.ecommerce.userInfo.DTO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record UserProductReviewInfo(
-        Long productId,
-        String thumbnail,
-        String productName,
-        String comment,
-        String reviewMediaURL,
-        @Valid @NotNull
-        Integer rating,
-        @Valid @NotBlank
-        String reviewTitle
-) {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProductReviewInfo {
+
+    private Long productId;
+    private String thumbnail;
+    private String productName;
+    private String comment;
+    private String reviewMediaURL;
+    @Valid @NotNull
+    private Integer rating;
+    @Valid @NotBlank
+    private String reviewTitle;
+
+    public UserProductReviewInfo(Long productId, String thumbnail, String productName) {
+        this.productId = productId;
+        this.thumbnail = thumbnail;
+        this.productName = productName;
+    }
 }
