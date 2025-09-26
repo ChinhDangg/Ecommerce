f27721cd6348615efb7c9032362e95470be409a7
chinhdang <dangchinh645@gmail.com>
2025-09-26 19:53:16 -0400

    add sign out api, and minor refactor with checking info for registering

--- Backend/src/main/java/dev/ecommerce/auth/AuthenticationController.java
+++ Backend/src/main/java/dev/ecommerce/auth/AuthenticationController.java
@@ -1,8 +1,10 @@
 package dev.ecommerce.auth;
 
+import dev.ecommerce.auth.jwt.JwtService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,8 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public void register(@RequestBody RegisterRequest registerRequest, HttpServletResponse response) {
-        if (!registerRequest.haveAllFields()) {
-            throw new IllegalArgumentException("Missing required fields");
+        if (!registerRequest.checkAllFields()) {
+            throw new IllegalArgumentException("Fields requirement not met.");
         }
         AuthenticationResponse authenticationResponse = authenticationService.register(registerRequest);
         response.setHeader("Location", gatewayURL);
@@ -45,5 +47,11 @@ public class AuthenticationController {
         response.addHeader(HttpHeaders.SET_COOKIE, authenticationResponse.cookie().toString());
         //response.addCookie(authenticationResponse.cookie());
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @PostMapping("/sign-out")
+    public void signOut(HttpServletResponse response) {
+        response.addCookie(JwtService.removeAuthCookie());
+        response.setStatus(HttpStatus.NO_CONTENT.value());
     }
 }
