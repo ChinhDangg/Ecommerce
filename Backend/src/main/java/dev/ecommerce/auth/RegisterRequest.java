1b4de20066b40c5037c460156ca6de8f1db065f0
chinhdang <dangchinh645@gmail.com>
2025-09-26 21:19:04 -0400

    update register request dto

--- Backend/src/main/java/dev/ecommerce/auth/RegisterRequest.java
+++ Backend/src/main/java/dev/ecommerce/auth/RegisterRequest.java
@@ -1,16 +1,35 @@
 package dev.ecommerce.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.regex.Pattern;
+
 public record RegisterRequest(
-    String firstname,
-    String lastname,
-    String username,
-    String password
+    @NotBlank @Length(max = 20)
+    String firstName,
+    @NotBlank @Length(max = 20)
+    String lastName,
+    @NotBlank @Length(max = 20)
+    String email,
+    @NotBlank @Length(min = 8, max = 30)
+    String password,
+    @NotBlank @Length(min = 8, max = 30)
+    String confirmPassword
 ) {
 
-    public boolean haveAllFields() {
-        return firstname != null && !firstname.isEmpty() &&
-                lastname != null && !lastname.isEmpty() &&
-                username != null && !username.isEmpty() &&
-                password != null && !password.isEmpty();
+    public boolean checkAllFields() {
+        return checkPassword();
+    }
+
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public boolean checkPassword() {
+        if (!password.equals(confirmPassword))
+            return false;
+        return pattern.matcher(password).matches();
     }
 }
